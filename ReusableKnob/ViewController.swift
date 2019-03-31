87a357613843dc560147d6eefa68c9caf523242e
David Dinh <dmdinh22@gmail.com>
2019-03-30 17:19:47 -0700

    create knob and knob outlet to VC

--- ReusableKnob/ViewController.swift
+++ ReusableKnob/ViewController.swift
@@ -33,7 +33,8 @@ class ViewController: UIViewController {
   @IBOutlet var valueLabel: UILabel!
   @IBOutlet var valueSlider: UISlider!
   @IBOutlet var animateSwitch: UISwitch!
-
+  @IBOutlet weak var knob: Knob!
+    
   override func viewDidLoad() {
     super.viewDidLoad()
   }
